e6c5b5cec6b146561eaf2faee73e5f13db6fa277
Igor Bobko <limit-speed@yandex.ru>
2016-04-04 22:27:44 +0300

    Wall items

--- src/main/java/ru/todo100/activer/controller/WallController.java
+++ src/main/java/ru/todo100/activer/controller/WallController.java
@@ -1,8 +1,11 @@
 package ru.todo100.activer.controller;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.GregorianCalendar;
 
+import org.json.JSONObject;
+import org.json.JSONStringer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,18 +13,26 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.todo100.activer.data.MessageData;
+import ru.todo100.activer.data.ReceiveWallData;
 import ru.todo100.activer.model.AccountItem;
 import ru.todo100.activer.model.WallItem;
 import ru.todo100.activer.populators.WallPopulator;
 import ru.todo100.activer.dao.AccountDao;
 import ru.todo100.activer.dao.WallDao;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
- * @author Igor Bobko
+ * @author Igor Bobko <limit-speed@yandex.ru>.
  */
 @Controller
-public class WallWebSocketController
+@RequestMapping("/wall")
+public class WallController
 {
 	@Autowired
 	private AccountDao accountService;
@@ -32,26 +43,21 @@ public class WallWebSocketController
 	@Autowired
 	private WallDao wallService;
 
-	@MessageMapping("/wall/{id}")
-	@SendTo("/wall1/{id}")
-	public MessageData listenWall(@DestinationVariable final Integer id, final MessageData messageData,Principal principal)
-	{
-		if (StringUtils.isEmpty(messageData.getText())) {
-			return new MessageData();
-		}
 
-		final AccountItem account = accountService.get(id);
-		final AccountItem currentAccount = accountService.get(principal.getName());
+	@RequestMapping("/publish")
+	@ResponseBody
+	public void publish(@ModelAttribute ReceiveWallData receiveWallData, BindingResult bindingResult, HttpServletResponse response) throws IOException {
+		final AccountItem account = accountService.get(receiveWallData.getId());
+		final AccountItem currentAccount = accountService.getCurrentAccount();
 		WallItem post = new WallItem();
 		post.setAccount(account);
-		post.setText(messageData.getText());
+		post.setText(receiveWallData.getText());
 		post.setAddedDate(new GregorianCalendar());
 		post.setSender(currentAccount.getId());
-
 		wallService.save(post);
-		return wallPopulator.populate(post);
+		final JSONObject jsonObject = new JSONObject(wallPopulator.populate(post));
+		response.getWriter().println(jsonObject.toString());
 	}
-
 }
 
 
