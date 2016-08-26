36c5b464ed5de5868ceeba2216de7cf391ad04f3
Igor Bobko <limit-speed@yandex.ru>
2016-08-27 01:41:50 +0300

    Added Video page

--- src/main/java/ru/todo100/activer/controller/VideosPageController.java
+++ src/main/java/ru/todo100/activer/controller/VideosPageController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.todo100.activer.dao.AccountDao;
 import ru.todo100.activer.dao.VideoDao;
 import ru.todo100.activer.form.VideoForm;
@@ -15,6 +12,8 @@ import ru.todo100.activer.model.VideoItem;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Igor Bobko <limit-speed@yandex.ru>.
@@ -22,6 +21,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/videos")
 public class VideosPageController {
+    private static final Pattern YOUTUBE_LINK= Pattern.compile("^https://www.youtube.com/watch\\?v=(.+)$");
 
     @Autowired
     private AccountDao accountService;
@@ -53,13 +53,17 @@ public class VideosPageController {
 
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
     public String save(final VideoForm videoForm, final BindingResult bindingResult) {
-        VideoItem videoItem = new VideoItem();
-        Integer accountId = accountService.getCurrentAccount().getId();
-        videoItem.setAccountId(accountId);
-        videoItem.setBody(videoForm.getBody());
-        videoItem.setDescription(videoForm.getDescription());
-        videoDao.save(videoItem);
-
+        Matcher m = YOUTUBE_LINK.matcher( videoForm.getBody());
+        if (m.matches()) {
+            String id = m.group(1);
+            String iframe = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/"+id+"\" frameborder=\"0\" allowfullscreen></iframe>";
+            VideoItem videoItem = new VideoItem();
+            Integer accountId = accountService.getCurrentAccount().getId();
+            videoItem.setAccountId(accountId);
+            videoItem.setBody(iframe);
+            videoItem.setDescription(videoForm.getDescription());
+            videoDao.save(videoItem);
+        }
         return "redirect:/videos";
     }
 
