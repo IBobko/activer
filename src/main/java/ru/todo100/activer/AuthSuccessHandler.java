e6668eecd072dd091b43e6d64cdaf17167dfcd6e
Igor Bobko <limit-speed@yandex.ru>
2016-09-17 16:23:55 +0300

    Cleaning

--- src/main/java/ru/todo100/activer/AuthSuccessHandler.java
+++ src/main/java/ru/todo100/activer/AuthSuccessHandler.java
@@ -16,42 +16,51 @@ import java.io.IOException;
 /**
  * @author Igor Bobko <limit-speed@yandex.ru>.
  */
-public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler
-{
-	private AccountDao accountService;
+public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    private AccountDao accountService;
+    private FriendsService friendsService;
+    private MessageDao messageService;
 
-	@Autowired
-	private FriendsService friendsService;
+    public FriendsService getFriendsService() {
+        return friendsService;
+    }
 
-	@Autowired
-	private MessageDao messageService;
+    @Autowired
+    public void setFriendsService(FriendsService friendsService) {
+        this.friendsService = friendsService;
+    }
 
+    public MessageDao getMessageService() {
+        return messageService;
+    }
 
-	public AccountDao getAccountService() {
-		return accountService;
-	}
-	@Autowired
-	public void setAccountService(AccountDao accountService) {
-		this.accountService = accountService;
-	}
+    @Autowired
+    public void setMessageService(MessageDao messageService) {
+        this.messageService = messageService;
+    }
 
-	@Override
-	public void onAuthenticationSuccess(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse,
-	                                    final Authentication authentication) throws IOException, ServletException
-	{
+    public AccountDao getAccountService() {
+        return accountService;
+    }
 
+    @Autowired
+    public void setAccountService(AccountDao accountService) {
+        this.accountService = accountService;
+    }
 
-		getAccountService().initCurrentProfile(httpServletRequest.getSession());
-		/*@todo возможно не стоит этот методо все таки вызывать здесь*/
-		httpServletRequest.setAttribute("friendsData", friendsService.getFriendData(httpServletRequest.getSession()));
-		httpServletRequest.getSession().setAttribute("unreadMessages",messageService.unreadCount(accountService.getCurrentProfileData(httpServletRequest.getSession()).getId()));
-		if (httpServletRequest.getParameter("remember-me") != null) {
-			Cookie cookie = new Cookie("remember-me", "1");
-			cookie.setMaxAge(604800);
-			cookie.setPath("/");
-			httpServletResponse.addCookie(cookie);
-		}
-
-		super.onAuthenticationSuccess(httpServletRequest,httpServletResponse,authentication);
-	}
+    @Override
+    public void onAuthenticationSuccess(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse,
+                                        final Authentication authentication) throws IOException, ServletException {
+        getAccountService().initCurrentProfile(httpServletRequest.getSession());
+        /*@todo возможно не стоит этот методо все таки вызывать здесь*/
+        httpServletRequest.setAttribute("friendsData", getFriendsService().getFriendData(httpServletRequest.getSession()));
+        httpServletRequest.getSession().setAttribute("unreadMessages", getMessageService().unreadCount( getAccountService().getCurrentProfileData(httpServletRequest.getSession()).getId()));
+        if (httpServletRequest.getParameter("remember-me") != null) {
+            Cookie cookie = new Cookie("remember-me", "1");
+            cookie.setMaxAge(604800);
+            cookie.setPath("/");
+            httpServletResponse.addCookie(cookie);
+        }
+        super.onAuthenticationSuccess(httpServletRequest, httpServletResponse, authentication);
+    }
 }
