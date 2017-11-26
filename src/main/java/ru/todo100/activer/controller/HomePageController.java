63e95ab35a99955df87c140d956afc1c2705198e
Igor Bobko <limit-speed@yandex.ru>
2017-11-26 13:06:22 +0300

    added file for payeer.

--- src/main/java/ru/todo100/activer/controller/HomePageController.java
+++ src/main/java/ru/todo100/activer/controller/HomePageController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 @Controller
 @RequestMapping
@@ -19,9 +22,11 @@ public class HomePageController
 
 	@ResponseBody
 	@RequestMapping("/payeer_433665898.txt")
-	public String txt(HttpServletResponse response)
-	{
-		response.setHeader("Content-Disposition","attachment; filename=\"payeer_433665898.txt\"");
-		return "1";
+	public String txt(HttpServletResponse response) throws UnsupportedEncodingException {
+		String fileName = URLEncoder.encode("payeer_433665898.txt", "UTF-8");
+		fileName = URLDecoder.decode(fileName, "ISO8859_1");
+		response.setContentType("application/x-msdownload");
+		response.setHeader("Content-Disposition","attachment; filename=\"" + fileName + "\"");
+		return "payeer_433665898.txt";
 	}
 }
