a31c07d27799e116b37ac8df7efde054f9709699
Igor Bobko <limit-speed@yandex.ru>
2017-12-12 16:33:39 +0300

    moved tiles directory.

--- src/main/java/ru/todo100/activer/Application.java
+++ src/main/java/ru/todo100/activer/Application.java
@@ -67,7 +67,7 @@ public class Application extends SpringBootServletInitializer {
     @Bean
     public TilesConfigurer tilesConfigurer() {
         TilesConfigurer tilesConfigurer = new TilesConfigurer();
-        tilesConfigurer.setDefinitions("/WEB-INF/tiles/tiles-templates.xml");
+        tilesConfigurer.setDefinitions("classpath:/tiles/tiles-templates.xml");
         return tilesConfigurer;
     }
 
